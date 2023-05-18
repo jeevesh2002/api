eae5107a2e106d9015bdcb2a00a85a2f51fc8f75
Jeevesh Krishna <a.jeeveshkrishna@gmail.com>
2023-05-19 05:18:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, send_file, request, jsonify
+from flask import render_template, url_for
+import json
+app = Flask(__name__)
+
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/')
+def index():
+    routes = []
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint != 'static':
+            routes.append(f'<a href="{rule}">{rule}</a>')
+    return '<br>'.join(routes)
+
+@app.route('/resume', methods=['GET', 'POST'])
+def resume():
+    if request.method == 'POST':
+        return send_file('static/resume.pdf', as_attachment=True)
+    else:
+        return send_file('static/resume.pdf', as_attachment=True)
+    
+
+@app.route('/personal-info')
+def personal_info():
+    with open('static/personal-info.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route('/skills')
+def skills():
+    with open('static/skills.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+
+@app.route('/contact')
+def contact():
+    with open('static/contact.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route('/education')
+def education():
+    with open('static/education.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route('/experience')
+def experience():
+    with open('static/experience.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route('/profiles')
+def profiles():
+    with open('static/profiles.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
